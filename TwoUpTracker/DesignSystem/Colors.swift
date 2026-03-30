869062e1a1402fe385a554ef071a6ee534dc5683
Alex Skorulis <skorulis@gmail.com>
2026-03-31 10:26:15 +1100

    Add Australia colors

--- TwoUpTracker/DesignSystem/Colors.swift
+++ TwoUpTracker/DesignSystem/Colors.swift
@@ -3,7 +3,7 @@ import UIKit
 
 /// Semantic colors aligned with PRD: system colors, clear profit vs loss.
 enum Colors {
-    static var profit: Color { .green }
+    static var profit: Color { australianGreen }
     static var loss: Color { .red }
     static var neutral: Color { .secondary }
 
@@ -14,4 +14,13 @@ enum Colors {
     static var groupedBackground: Color {
         Color(uiColor: .systemGroupedBackground)
     }
+
+    // MARK: - Australian national colours
+    // Reference: Pantone 348 C and 116 C (Australian Government); common sRGB approximations #00843D / #FFCD00.
+
+    /// Australian national green (Pantone 348 C, sRGB `#00843D`).
+    static let australianGreen = Color(red: 0, green: 132.0 / 255, blue: 61.0 / 255)
+
+    /// Australian national gold (Pantone 116 C, sRGB `#FFCD00`).
+    static let australianGold = Color(red: 1, green: 205.0 / 255, blue: 0)
 }
