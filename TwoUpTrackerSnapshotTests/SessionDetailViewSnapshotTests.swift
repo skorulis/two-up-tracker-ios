8cf744b1a35256073f98fd95e40e3efb821e9676
Alex Skorulis <skorulis@gmail.com>
2026-03-31 08:30:16 +1100

    Add snapshots to session details

---
+++ TwoUpTrackerSnapshotTests/SessionDetailViewSnapshotTests.swift
@@ -0,0 +1,34 @@
+@testable import TwoUpTracker
+import Knit
+import SnapshotTesting
+import SwiftUI
+import Testing
+
+@MainActor @Suite(.snapshots(record: .failed))
+struct SessionDetailViewSnapshotTests {
+
+    private let assembler = TwoUpTrackerAssembly.testing()
+    private let testData = TestData()
+
+    @Test func sessionDetailWithRealisticData() async throws {
+        let viewModel = assembler.resolver.sessionDetailViewModel()
+        viewModel.mainStore.activeSession = testData.realisticSession()
+
+        let view = NavigationStack {
+            SessionDetailView(model: viewModel)
+        }
+
+        assertSnapshot(of: view, as: .image(on: .iPhoneSe))
+    }
+
+    @Test func sessionDetailWithRealisticDataDarkMode() async throws {
+        let viewModel = assembler.resolver.sessionDetailViewModel()
+        viewModel.mainStore.activeSession = testData.realisticSession()
+
+        let view = NavigationStack {
+            SessionDetailView(model: viewModel)
+        }
+
+        assertSnapshot(of: view, as: .image(on: .iPhoneSe), style: .dark)
+    }
+}
